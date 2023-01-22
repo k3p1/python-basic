80532f5dff3133a1b5cf530722a1e2a49d0862db
k3p1 <v.lopaevv@gmail.com>
2023-01-23 04:03:04 +0500

    Add class Car in car.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -1,3 +1,15 @@
 """
 создайте класс `Car`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.engine import Engine
+
+
+class Car(Vehicle):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.engine = None
+
+    def set_engine(self):
+        self.engine = Engine
+
