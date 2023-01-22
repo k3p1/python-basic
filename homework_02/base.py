b97f9278f7c29fa2775e4cb6edb7d4be290ee6ba
k3p1 <v.lopaevv@gmail.com>
2023-01-23 03:01:31 +0500

    Add function start

--- homework_02/base.py
+++ homework_02/base.py
@@ -2,4 +2,17 @@ from abc import ABC
 
 
 class Vehicle(ABC):
-    pass
+    weight = 0
+    started = False
+    fuel = 10
+    fuel_consumption = 0
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = 0
+        self.fuel = 0
+        self.fuel_consumption = 0
+
+    def start(self):
+        if not self.started:
+            if self.fuel > 0:
+                self.started = True
