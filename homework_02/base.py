fd424e68988481bb9ec29e38beeca928e1aea21c
k3p1 <v.lopaevv@gmail.com>
2023-01-26 03:04:23 +0500

    Add ValueError

--- homework_02/base.py
+++ homework_02/base.py
@@ -4,16 +4,13 @@ from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 class Vehicle(ABC):
     def __init__(self, weight=10, fuel=10, fuel_consumption=2):
-        # if weight or fuel or fuel_consumption >= 0:
-        #     self.weight = weight
-        #     self.fuel = fuel
-        #     self.fuel_consumption = fuel_consumption
-        # else:
-        #     raise ValueError
-        self.weight = weight
-        self.fuel = fuel
-        self.fuel_consumption = fuel_consumption
         self.started = False
+        if weight or fuel or fuel_consumption >= 0:
+            self.weight = weight
+            self.fuel = fuel
+            self.fuel_consumption = fuel_consumption
+        else:
+            raise ValueError("weight, fuel, fuel_consumption не может быть меньше 0")
 
     def start(self):
         if not self.started:
@@ -23,14 +20,10 @@ class Vehicle(ABC):
                 raise LowFuelError
 
     def move(self, distance):
-        # if distance >= 0:
-        #     if self.fuel >= distance * self.fuel_consumption:
-        #         self.fuel -= distance * self.fuel_consumption
-        #     else:
-        #         raise NotEnoughFuel
-        # else:
-        #     raise ValueError
-        if self.fuel >= distance * self.fuel_consumption:
-            self.fuel -= distance * self.fuel_consumption
+        if distance >= 0:
+            if self.fuel >= distance * self.fuel_consumption:
+                self.fuel -= distance * self.fuel_consumption
+            else:
+                raise NotEnoughFuel
         else:
-            raise NotEnoughFuel
+            raise ValueError("distance не может быть меньше нуля")
